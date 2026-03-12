b801dc71ecdcbdbd4bf8e390b060cdca792a1904
Yasemin <ecenurlum@gmail.com>
2026-03-12 10:34:21 -0400

    helpppp

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ print("idk what im doing")
 print("this is a new line - abby commit")
 print("this is a new line - akilah commit")
 print("teammmmm")
-print("HELLO WORLD - ABBY")
-print("YURRR")
 
 #fml again and again and again
 
